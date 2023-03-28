163eee769021ad9735963f5274e482af4352373c
craigkelleher <kelleher.craig@gmail.com>
2023-03-28 14:14:51 -0500

    Implement StocksController with GET Route

---
+++ src/main/java/com/example/mockstock/stocks/StocksController.java
@@ -0,0 +1,36 @@
+package com.example.mockstock.stocks;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+@RestController
+public class StocksController {
+    private final String API_KEY;
+
+    public StocksController(String API_KEY){
+        this.API_KEY = API_KEY;
+    }
+
+    @GetMapping("/quotes")
+    public String getQuotes() throws Exception {
+        String url = "https://finnhub.io/api/v1/quote?symbol=AAPL&token=API_KEY";
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(url);
+        CloseableHttpResponse httpResponse = httpClient.execute(httpGet);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(
+                httpResponse.getEntity().getContent()));
+
+        StringBuilder result = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            result.append(line);
+        }
+        return result.toString();
+    }
+}
