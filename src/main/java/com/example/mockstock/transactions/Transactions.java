2e1e982ef759959ce14f69cc58e3bfe0f71354d3
Jeremy Linder <jeremylinder2@gmail.com>
2023-03-29 07:46:55 -0700

    resolved bug in transaction file and built transaction entity

--- src/main/java/com/example/mockstock/transactions/Transactions.java
+++ src/main/java/com/example/mockstock/transactions/Transactions.java
@@ -1,6 +1,65 @@
 package com.example.mockstock.transactions;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
+@Entity
 public class Transactions {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "user_id")
+    private Long userId;
+    @Column(name = "stock_symbol")
+    private String stockSymbol;
+    private String transactionType;
+    private Double stockPrice;
+    private int quantity;
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getStockPrice() {
+        return stockPrice;
+    }
+
+    public void setStockPrice(Double stockPrice) {
+        this.stockPrice = stockPrice;
+    }
+
+    public String getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
+
+    public String getStockSymbol() {
+        return stockSymbol;
+    }
+
+    public void setStockSymbol(String stockSymbol) {
+        this.stockSymbol = stockSymbol;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
