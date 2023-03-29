c61e2d9af8a8ba2570a62f577dc0070f67647336
craigkelleher <kelleher.craig@gmail.com>
2023-03-29 10:27:15 -0500

    Create String Bean for API KEY

--- src/main/java/com/example/mockstock/MockstockApplication.java
+++ src/main/java/com/example/mockstock/MockstockApplication.java
@@ -3,13 +3,15 @@ package com.example.mockstock;
 import com.example.mockstock.stocks.StocksController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class MockstockApplication {
 
 	public static void main(String[] args) throws Exception {
-		SpringApplication.run(MockstockApplication.class, args);
-		StocksController stocksController = new StocksController(System.getProperty("API_KEY"));
-		System.out.println("Apple quote: " + stocksController.getQuote("AAPL"));
+		ApplicationContext context = SpringApplication.run(MockstockApplication.class, args);
+		StocksController stocksController = context.getBean(StocksController.class);
+		String quote = stocksController.getQuote("AAPL");
+		System.out.println(quote);
 	}
 }
