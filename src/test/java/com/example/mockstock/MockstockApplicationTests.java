c13c97306120d20b63fa89cd0dc963552731c0b1
Jeremy Linder <jeremylinder2@gmail.com>
2023-03-30 09:40:18 -0700

    uncommented test

--- src/test/java/com/example/mockstock/MockstockApplicationTests.java
+++ src/test/java/com/example/mockstock/MockstockApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MockstockApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
+	@Test
+	void contextLoads() {
+	}
 
 }
