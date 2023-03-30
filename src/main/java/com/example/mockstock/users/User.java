ddb4cc5adf72d39e18492d2a79f3b48e5ac5a609
Jeremy Linder <jeremylinder2@gmail.com>
2023-03-30 09:31:26 -0700

    added password encrpytion stuff with spring security

--- src/main/java/com/example/mockstock/users/User.java
+++ src/main/java/com/example/mockstock/users/User.java
@@ -19,7 +19,7 @@ public class User {
     @Column (name = "user_name")
     private String name;
     private String email;
-    @Column (name = "user_password")
+    @Column (name = "user_password", columnDefinition = "VARCHAR(255) BINARY")
     private String password;
     private Double balance;
     @OneToMany(
